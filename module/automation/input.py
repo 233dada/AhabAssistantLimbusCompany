36214847b694374cb367a02cceaaacbdde3130ef
233dada <233dada@gmail.com>
2026-02-09 12:54:56 +0800

    chore: 删除依赖`pyautogui`的代码

--- module/automation/input.py
+++ module/automation/input.py
@@ -1,7 +1,6 @@
 import random
 from time import sleep, time
 
-import pyautogui
 import win32api
 import win32con
 import win32gui
@@ -64,7 +63,6 @@ class Input(metaclass=SingletonMeta):
     """基于 `pyautogui` 的输入类, 仅支持前台操作"""
 
     # 禁用pyautogui的失败安全特性，防止意外中断
-    pyautogui.FAILSAFE = False
 
     def __init__(self):
         self.is_pause = False
@@ -105,21 +103,7 @@ class Input(metaclass=SingletonMeta):
         Returns:
             bool (True) : 总是返回True表示操作执行完毕
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        msg = f"点击位置:({x},{y})"
-        log.debug(msg, stacklevel=2)
-        for i in range(times):
-            pyautogui.click(x, y)
-            # 多次点击执行很快所以暂停放到循环外
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
-
-        self.wait_pause()
-
-        return True
+        raise NotImplementedError()
 
     def mouse_drag_down(self, x, y, reverse=1, move_back=True) -> None:
         """鼠标从指定位置向下拖动
@@ -130,17 +114,7 @@ class Input(metaclass=SingletonMeta):
             reverse (int): 拖动方向，1表示向下，-1表示向上
             move_back (bool): 是否在拖动后将鼠标移动回原位置
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        scale = cfg.set_win_size / 1080
-        pyautogui.moveTo(x, y)
-        pyautogui.mouseDown()
-        pyautogui.dragTo(x, y + int(300 * scale * reverse), duration=0.4)
-        pyautogui.mouseUp()
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
+        raise NotImplementedError()
 
     def mouse_drag(self, x, y, drag_time=0.1, dx=0, dy=0, move_back=True) -> None:
         """鼠标从指定位置拖动到另一个位置
@@ -152,20 +126,7 @@ class Input(metaclass=SingletonMeta):
             dy (int): y方向拖动距离
             move_back (bool): 是否在拖动后将鼠标移动回原位置
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        pyautogui.moveTo(x, y)
-        pyautogui.mouseDown()
-        pyautogui.moveTo(x + dx, y + dy, duration=drag_time)
-        if drag_time * 0.3 > 0.5:
-            sleep(drag_time * 0.3)
-        else:
-            sleep(0.5)
-        pyautogui.mouseUp()
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
+        raise NotImplementedError()
 
     def mouse_scroll(self, direction: int = -3) -> bool:
         """
@@ -175,13 +136,7 @@ class Input(metaclass=SingletonMeta):
         Returns:
             bool (True) : 表示是否支持该操作
         """
-        if direction <= 0:
-            msg = "鼠标滚动滚轮，远离界面"
-        else:
-            msg = "鼠标滚动滚轮，拉近界面"
-        log.debug(msg, stacklevel=2)
-        pyautogui.scroll(direction)
-        return True
+        raise NotImplementedError()
 
     def mouse_click_blank(self, coordinate=(1, 1), times=1, move_back=False) -> bool:
         """在空白位置点击鼠标
@@ -192,21 +147,7 @@ class Input(metaclass=SingletonMeta):
         Returns:
             bool (True) : 总是返回True表示操作执行完毕
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        msg = "点击（1，1）空白位置"
-        log.debug(msg, stacklevel=2)
-        x = coordinate[0] + random.randint(0, 10)
-        y = coordinate[1] + random.randint(0, 10)
-        for i in range(times):
-            pyautogui.click(x, y)
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
-
-        self.wait_pause()
-        return True
+        raise NotImplementedError()
 
     def mouse_to_blank(self, coordinate=(1, 1), move_back=False) -> None:
         """鼠标移动到空白位置，避免遮挡
@@ -214,16 +155,7 @@ class Input(metaclass=SingletonMeta):
             coordinate (tuple): 坐标元组 (x, y)
             move_back (bool): 是否在移动后将鼠标移动回原位置
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        msg = "鼠标移动到空白，避免遮挡"
-        log.debug(msg, stacklevel=2)
-        pyautogui.moveTo(coordinate[0], coordinate[1])
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
-        self.wait_pause()
+        raise NotImplementedError()
 
     def mouse_move(self, coordinate=(1, 1)) -> None:
         """鼠标移动到指定坐标
@@ -231,8 +163,7 @@ class Input(metaclass=SingletonMeta):
         Args:
             coordinate (tuple): 坐标元组 (x, y)
         """
-        pyautogui.moveTo(coordinate[0], coordinate[1])
-        self.wait_pause()
+        raise NotImplementedError()
 
     def get_mouse_position(self) -> tuple[int, int]:
         """获取鼠标当前位置
@@ -240,7 +171,7 @@ class Input(metaclass=SingletonMeta):
         Returns:
             tuple: 当前鼠标位置的元组 (x, y)
         """
-        return pyautogui.position()
+        raise NotImplementedError()
 
     def mouse_drag_link(self, position: list, drag_time=0.1, move_back=False) -> None:
         """鼠标从指定位置拖动到指定位置
@@ -250,20 +181,10 @@ class Input(metaclass=SingletonMeta):
             position (list): 目标位置列表
             drag_time (float): 拖动时间
         """
-        if move_back:
-            current_mouse_position = self.get_mouse_position()
-
-        pyautogui.moveTo(position[0][0], position[0][1])
-        pyautogui.mouseDown()
-        for pos in position:
-            pyautogui.moveTo(pos[0], pos[1], duration=drag_time)
-        pyautogui.mouseUp()
-
-        if move_back and current_mouse_position:
-            self.mouse_move(current_mouse_position)
+        raise NotImplementedError()
 
     def key_press(self, key):
-        return pyautogui.press(key)
+        raise NotImplementedError()
 
 
 class BackgroundInput(Input, metaclass=SingletonMeta):
@@ -567,9 +488,9 @@ class BackgroundInput(Input, metaclass=SingletonMeta):
         except PyWinTypesError as e:
             # 奇怪的权限冲突 (183:当文件已存在时，无法创建该文件。)
             # 偶尔出现不影响使用
-
             log.debug(f"鼠标移动失败: {e}")
+            sleep(0.5)
             try:
-                pyautogui.moveTo(x, y)
-            except Exception as e:
-                log.error(f"鼠标移动失败: {type(e)}: {e}")
+                win32api.SetCursorPos((x, y))
+            except PyWinTypesError as e2:
+                log.error(f"鼠标移动失败: {e2}")
--- module/automation/screenshot.py
+++ module/automation/screenshot.py
@@ -3,7 +3,6 @@ from ctypes import windll
 
 import cv2
 import numpy as np
-import pyautogui
 import pywintypes
 import win32gui
 import win32ui
@@ -46,14 +45,9 @@ class ScreenShot:
             try:
                 return ScreenShot.take_screenshot_gdi(gray)
             except Exception as e:
-                msg = f"GDI截图失败，尝试使用pyautogui截图，错误信息：{e}"
-                log.debug(msg)
-                try:
-                    return ScreenShot.take_screenshot_pyautogui(gray)
-                except Exception as e2:
-                    msg = f"pyautogui截图失败，错误信息：{e2}"
-                    log.debug(msg)
-                    return None
+                msg = f"GDI截图失败，错误信息：{e}"
+                log.error(msg)
+                return None
 
     @staticmethod
     def take_screenshot_gdi(gray: bool = True) -> Image.Image:
@@ -125,43 +119,6 @@ class ScreenShot:
         image = image.crop((0, 0, size_width, size_height))
 
         return image
-
-    @staticmethod
-    def take_screenshot_pyautogui(gray: bool = True) -> Image.Image:
-        """
-        截取屏幕截图,使用pyautogui。
-        Args:
-            gray (bool): 是否将图片转化为灰度图
-        Returns:
-            screenshot: 截取的屏幕截图。
-        """
-        # 根据配置获取窗口的高度
-        size_height = cfg.set_win_size
-        # 计算窗口的宽度，保持16:9的宽高比
-        size_width = size_height / 9 * 16
-
-        """# 如果move参数为True，则尝试移动鼠标到屏幕左上角
-        if move:
-            try:
-                pyautogui.moveTo(1, 1)
-            except:
-                pass"""
-
-        # 设置进程的DPI感知，以确保截图在不同DPI设置下正确显示
-        windll.user32.SetProcessDPIAware()
-        # 进行全屏截图
-        screenshot_temp = pyautogui.screenshot()
-        if gray:
-            # 将截图转换为灰度图像
-            screenshot = screenshot_temp.convert("L")
-        else:
-            screenshot = screenshot_temp
-
-        # 裁剪截图到指定的宽高
-        screenshot = screenshot.crop((0, 0, size_width, size_height))
-
-        # 返回裁剪后的截图
-        return screenshot
 
     @staticmethod
     def background_screenshot(gray: bool = True) -> Image.Image:
